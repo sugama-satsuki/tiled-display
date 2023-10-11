5f6c754449258bb5e66625fa6037e02efded029f
sugama-satsuki <piguchi@geolonia.com>
2023-10-11 16:23:20 +0900

    タイル生成修正

--- .github/tile.sh
+++ .github/tile.sh
@@ -10,57 +10,50 @@ tippecanoe -v
 
 set -ex
 
-# execlファイルからcsvファイルを生成
-function excelToCsv() {
-    filename=$(basename $1 .xlsx)
-    csv=$filename".csv"
-    xlsx2csv $1 $csv
-}
+cd ./data
+xlsx2csv 12_suisyou_koukyoushisetu-1.xlsx 12_suisyou_koukyoushisetu-1.csv
+sed -i'' "s/緯度/lat/" 12_suisyou_koukyoushisetu-1.csv
+sed -i'' "s/経度/lon/" 12_suisyou_koukyoushisetu-1.csv
+ogr2ogr -f GeoJSON 12_suisyou_koukyoushisetu-1".geojson" $1 -oo X_POSSIBLE_NAMES=lat* -oo Y_POSSIBLE_NAMES=lon*
+tippecanoe -zg -o 'sample.mbtiles' *.geojson
 
-# csvファイルからgeojsonファイルを生成
-function csvToGeojson() {
-    filename=$(basename $1 .csv)
-    geojson=$filename".geojson"
-    sed -i'' "s/緯度/lat/" $1
-    sed -i'' "s/経度/lon/" $1
 
-    ogr2ogr -f GeoJSON $geojson $1 -oo X_POSSIBLE_NAMES=lat* -oo Y_POSSIBLE_NAMES=lon*
-    if [ ! -e ../geojson_data ]; then mkdir ../geojson_data ; fi
-    mv -f $geojson ../geojson_data
-}
+# # execlファイルからcsvファイルを生成
+# function excelToCsv() {
+#     filename=$(basename $1 .xlsx)
+#     xlsx2csv $1 $filename".csv"
+# }
 
-# geojsonファイルからpmtilesを生成
-function geojsonToTile() {
-    fileName='sample.mbtiles'
-    tippecanoe -zg -o $fileName ../geojson_data/*
-    pwd
-}
+# # csvファイルからgeojsonファイルを生成
+# function csvToGeojson() {
+#     filename=$(basename $1 .csv)
+#     sed -i'' "s/緯度/lat/" $1
+#     sed -i'' "s/経度/lon/" $1
 
+#     ogr2ogr -f GeoJSON $filename".geojson" $1 -oo X_POSSIBLE_NAMES=lat* -oo Y_POSSIBLE_NAMES=lon*
+# }
+
+# # geojsonファイルからpmtilesを生成
+# function geojsonToTile() {
+#     fileName='sample.mbtiles'
+#     tippecanoe -zg -o $fileName *.geojson
+#     pwd
+# }
 
 # タイルの生成
-function makeTile(){
+# function makeTile(){
+#     cd ./data
+#     ls | while read line
+#     do
+#         extension="."${line##*.}
+#         filename=$(basename $line $extension)
 
-    cd ./data
-
-    if [ -e ./geojson_data ]; then rm -rf ./geojson_data ; fi
-    if [ -e ./tile_data ]; then rm -rf ./tile_data ; fi    
-
-    ls | while read line
-    do
-        extension="."${line##*.}
-        filename=$(basename $line $extension)
-
-        case $extension in
-            ".xlsx") echo "1"$line
-                    excelToCsv $line
-                    csvToGeojson $filename".csv";;
-            ".csv") echo $line
-                    csvToGeojson $line;;
-        esac
-    done
-
-    geojsonToTile
-
-}
-
-makeTile
+#         case $extension in
+#             ".xlsx") excelToCsv $line
+#                     csvToGeojson $filename".csv";;
+#             ".csv") csvToGeojson $line;;
+#         esac
+#     done
+#     geojsonToTile
+# }
+# makeTile
